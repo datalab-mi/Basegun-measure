11611a6dbbe220a2e59b22598a3f74fdde562328
theophanedb <theophanedelbauffe@orange.fr>
2022-07-27 12:31:00 +0000

    Update utils.py, main.py

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import sys
-from utils import apply_SCRN, measure_length
+from utils import BasegunV3
 
 
-def main(path: str) -> None:
 
-    image_root = path # path of the folder containing images
+def main(images_root='./data/input_images', save_root='./data/segmented_images') -> None:
+    """ Return the images with the corresponding gun length
+    Args:
+        images_root (str): path of the folder containing all images
+        save_root (str): path of the folder that will contain the segmented images (segmentation by SCRN)
+    """
     
-    infos = apply_SCRN(image_root) # dictionnary like => {i : {path of segmented image, original size of the image, preprocessed image } }
-    
-    lengths = {}
-    
-    for i in infos.keys(): # for each segmented image, we compute the total length of the gun
-        lengths[i] = measure_length(infos[i])
+    lengths = BasegunV3(images_root, save_root)
         
-
+        
         
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import os
 
 from datetime import datetime
-import matplotlib.pyplot as plt
 import numpy as np
 
 import cv2
@@ -13,70 +12,91 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 from SCRN.model.ResNet_models import SCRN
-from SCRN.utils.data import test_dataset, SCRN_SIZE
+
+SCRN_SIZE = 352
 
 
 
-def apply_SCRN(image_root):
-    """ Load SCRN model with parameters and then apply it on the images of the folder 
+def prepare_image(image_path: str) -> torch.Tensor:
+    """Convert a PIL Image to model-compatible input
     Args:
-        image_root (str): path of the folder containing images
+        image_path (str): path of the input image
     Returns:
-        infos (dict): contains pre-processed image, path of the segmented image and the original size
-    """
+        t_image (torch.Tensor): converted image, shape = (3, SCRN_SIZE, SCRN_SIZE), normalized on ImageNet
+        original_size (list): list containing the original size (width, height) of the image (int, int)
+    """        
+    img_transform = transforms.Compose([
+            transforms.Resize((SCRN_SIZE, SCRN_SIZE)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]) # 3 values of mean and std : for each channel of RGB
     
+    with open(path, 'rb') as f:
+        im = Image.open(f)
+        image = im.convert('RGB')
+    
+    original_size = image.size
+    t_image = img_transform(image).unsqueeze(0)
+    
+    return t_image, original_size
+
+    
+    
+def load_SCRN():
+    """ Load SCRN model from its .pth file
+    Returns:
+        model (Model): loaded model ready for prediction
+    """    
     device = torch.device('cpu')
     model = SCRN()
     model.load_state_dict(torch.load('./SCRN/model/model.pth', map_location=device))
     model.to(device)
     model.eval()
-
-    save_path = './SCRN/saliency_maps/save_test/'
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-
-    test_loader = test_dataset(image_root, image_size=SCRN_SIZE, n_images=len(os.listdir(image_root)))
-
-    infos = {}
-
-    with torch.no_grad():
-        for i in range(test_loader.size):
-            image, name, prev_size = test_loader.load_data() # returns the i-th preprocessed image (resized, tensor, normalized), its path name, and its original size
-
-            image = Variable(image).cpu()
-
-            res, edge = model(image) # apply SCRN on the preprocessed image
-
-            res = F.upsample(res, size=(prev_size[1],prev_size[0]), mode='bilinear', align_corners=True) # resizing the result
-
-            res = res.sigmoid().data.cpu().numpy().squeeze() # dtype : float32
-            res = skimage.img_as_ubyte(res) # dtype : uint8 (this type is needed by imagio.imwrite())
-
-            new_path = save_path + name + '.png'
-            imageio.imwrite(new_path, res)
-
-            infos[i] = {"path": new_path, "size": prev_size, "preproc_image": image} # ///////!\\\\\\\\ change key to new_path
-            # we store pre-processed image, path and original size in order to use them later
-            
-            
-            # ?? resize image before returning
-            # make all processing at the same time for lower complexity:
-            
-            
-            # measure_length(...) 
-    return infos
-
-
-
-def measure_length(infos_image):
     
-    orig = cv2.imread(os.path.join("/workspace/data/val/autre_epaule", infos_image["path"].replace('SCRN/saliency_maps/test//','').replace('png','jpg'))) # original image 
+    return model
+
+
+
+def apply_SCRN(model, image: torch.Tensor, image_name: str, original_size: list) -> str:
+    """ Apply SCRN on the image AND save the resulted image with its corresponding length
+    Args:
+        model (Model): loaded model
+        image (torch.Tensor): preprocessed image
+        image_name (str): name of the image in the folder
+        original_size (list): list containing the original size (width and height) of the image
+    Returns:
+        new_path (str): path of the segmentated image made by SCRN
+    """   
+    image = Variable(image).cpu()
+
+    res, _ = model(image) # apply SCRN on the preprocessed image
+    res = F.upsample(res, size=(original_size[1], original_size[0]), mode='bilinear', align_corners=True) # resizing the result
+    res = res.sigmoid().data.cpu().numpy().squeeze() # dtype : float32
+    res = skimage.img_as_ubyte(res) # dtype : uint8 (this type is needed by imagio.imwrite())
+
+    new_path = save_path + image_name + '.png'
+    imageio.imwrite(new_path, res)
+    
+    return new_path
+    
+
+    
+def measure_length(image_path: str, new_path: str, original_size: list, preproc_image: torch.Tensor) -> float:
+    """ Measure the length of the gun in the image
+    Args:
+        image_path (str): path of the original image
+        new_path (str): path of the segmented image
+        original_size (list): width and height of the original image (before preprocessing)
+        preproc_image (torch.Tensor): preprocessed tensor (resized, normalized)
+    Returns:
+        length (float): length of the gun in centimeters
+    """    
+    orig = cv2.imread(image_path) # original image
     orig = cv2.cvtColor(orig, cv2.COLOR_BGR2RGB)
 
-    im_preproc = infos_image["preproc_image"][0].permute(1,2,0).numpy().astype(np.uint8) # preprocessed image from SCRN
-    im_preproc = cv2.resize(im_preproc, (infos_image["size"][0], infos_image["size"][1]))
+    im_preproc = preproc_image[0].permute(1,2,0).numpy().astype(np.uint8) # preprocessed image from SCRN
+    im_preproc = cv2.resize(im_preproc, (original_size[0], original_size[1]))
 
-    im = cv2.imread(infos_image["path"]) # segmented image from SCRN
+    im = cv2.imread(new_path) # segmented image from SCRN
     # im = cv2.erode(im, kernel=np.ones((5,5), np.uint8), iterations=2)
 
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
@@ -142,3 +162,33 @@ def measure_length(infos_image):
         
     length = None
     return length
+
+
+
+def BasegunV3(images_root: str, save_root: str) -> dict:
+    """ Load and apply SCRN on each image after preprocessing AND compute the length of the gun in every image
+    Args:
+        images_root (str): path of the folder containing all images
+        save_root (str): path of the folder that will contain the segmented images (segmentation by SCRN)
+    Returns:
+        lengths (dict): contains the lengths (value) from each image (key)
+    """
+
+    if not os.path.exists(save_root):
+        os.makedirs(save_root)
+
+    model = load_model() # SCRN
+        
+    lengths = {}
+    
+    with torch.no_grad():
+        for image_name in os.listdir(images_root):
+            image_path = images_root + '/' + image_name
+            
+            preproc_image, original_size = prepare_image(image_path) # preprocessed tensor (resized, normalized) & original size of the image
+            
+            new_path = apply_SCRN(model, preproc_image, image_name, original_size) # path of the segmented image (segmentation made by SCRN)
+
+            lengths[image_path] = measure_length(image_path, new_path, original_size, preproc_image)
+            
+    return lengths
